68d93bbc5116777db6e3ee44af6fed5555651b2b
zamriibr <48670145+zamriibr@users.noreply.github.com>
2023-04-12 06:55:25 +0800

    Add files via upload

---
+++ esp32_now_gateway.ino
@@ -0,0 +1,52 @@
+/*
+  Daniel Carrasco
+  This and more tutorials at https://www.electrosoftcloud.com/
+*/
+#include <esp_now.h>
+#include <WiFi.h>
+static const char* ssid     = "StudyRoom";
+static const char* password = "afiqdaniel19";
+// Structure to keep the temperature and humidity data
+// Is also required in the client to be able to save the data directly
+typedef struct temp_humidity {
+  float temperature;
+  float humidity;
+};
+// Create a struct_message called myData
+temp_humidity dhtData;
+// callback function executed when data is received
+void OnRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&dhtData, incomingData, sizeof(dhtData));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  Serial.print("Temperature: ");
+  Serial.println(dhtData.temperature);
+  Serial.print("Humidity: ");
+  Serial.println(dhtData.humidity);
+}
+void setup() {
+  // Initialize Serial Monitor
+  Serial.begin(115200);
+  
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+  WiFi.setSleep(false);
+  WiFi.begin(ssid, password);
+  //check wi-fi is connected to wi-fi network
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+  
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("There was an error initializing ESP-NOW");
+    return;
+  }
+  
+  // Once the ESP-Now protocol is initialized, we will register the callback function
+  // to be able to react when a package arrives in near to real time without pooling every loop.
+  esp_now_register_recv_cb(OnRecv);
+}
+void loop() {
+}
